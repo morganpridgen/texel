ecfe9adc0236188c9e603e82f73bc8a2d3c3f8d1
CurlyMorgan <morgancpridgen@gmail.com>
2019-10-02 21:28:02 -0500

    Removed test binaries, fixed bug in new TXL_RenderQuad, and added new TXL_RenderQuad to solidfill test.

--- tests/solidfill/main.cpp
+++ tests/solidfill/main.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <TEXEL/texel.h>
+#include <cmath>
 
 bool init();
 void update();
@@ -34,6 +35,7 @@ void update() {
 
 void render() {
   TXL_RenderQuad({160, 90, 320, 180}, {1, 1, 1, 1});
+  for (float i = 0; i < 16.0f; i ++) TXL_RenderQuad(320, 180, 128.0f * cos(i * 3.14f / 32.0f), 128.0f * sin(i * 3.14f / 32.0f), {fmod(i, 2), 1.0f - fmod(i, 2), fmod(i, 2), 1});
   disp.refresh();
 }
 
